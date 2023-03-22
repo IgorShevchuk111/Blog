143c302a8b9ac0dbbab0ec2ab9554e801ea2879e
Igor Shevchuk <igorshevchuk111@gmail.com>
2023-03-22 15:11:07 +0000

    added admin-module and shered-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './shared/module/app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AdminLayoutComponent } from './admin/components/admin-layout/admin-layout.component';
