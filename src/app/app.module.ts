50901927a40bbf2473bfa7fd9af7998138b52d05
Igor Shevchuk <igorshevchuk111@gmail.com>
2023-03-22 14:45:43 +0000

    Created App-routinng, Admin-routing, main-layout, Admin-layout, home-page, create-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AdminLayoutComponent } from './admin/components/admin-layout/admin-layout.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AdminLayoutComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
