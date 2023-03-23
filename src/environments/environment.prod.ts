c9fee1b7fa20f912f8a0a6bcb34dfee9a30ad138
Igor Shevchuk <igorshevchuk111@gmail.com>
2023-03-23 16:17:13 +0000

    Created fbdb URL environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from "./interfaces";
+
+export const environment: Environment = {
+  production: true,
+  fbdb: 'https://blog-angular-material-default-rtdb.firebaseio.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+import { Environment } from "./interfaces";
+
+export const environment: Environment = {
+  production: false,
+  fbdb: 'https://blog-angular-material-default-rtdb.firebaseio.com/'
 };
 
 /*
