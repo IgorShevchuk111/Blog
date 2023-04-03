064a6a1c9bba31d3baf841ab51f424bbfe1a6fa9
Igor Shevchuk <igorshevchuk111@gmail.com>
2023-04-03 14:47:15 +0100

    wip

---
+++ src/app/admin/search-post.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { SharedService } from '../shared/services/shared.service';
+
+
+@Pipe({
+  name: 'searchPost'
+})
+export class SearchPostPipe implements PipeTransform {
+
+  constructor(
+    private shared: SharedService
+  ){}
+
+  transform(array: any[], search: string , field: string = 'title'): any[] {
+    if (!search.trim()) {
+      return array
+    } else {
+      return array.filter(el => {
+        return el[field as keyof typeof el].toLowerCase().includes(search.toLowerCase())
+      })
+    }
+  }
+
+}
