aefa0d0f8a2b7d51a25907dbcf8a5c1492aee11f
Igor Shevchuk <igorshevchuk111@gmail.com>
2023-04-05 17:23:04 +0100

    wip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import { Environment } from "./interfaces";
 
 export const environment: Environment = {
   production: true,
-  fbdb: 'https://blog-angular-material-default-rtdb.firebaseio.com/'
+  fbdb: 'https://blog-angular-material-default-rtdb.firebaseio.com/',
+  apiKey: 'AIzaSyAVbJGVY4RtZFic396Xars4r6yfD9C2aks'
 };
