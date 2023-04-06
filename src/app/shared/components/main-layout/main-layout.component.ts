beb638b97dbd0db4b0ef831efd8da3ca7e161e98
Igor Shevchuk <igorshevchuk111@gmail.com>
2023-04-06 21:59:53 +0100

    Sticky Navbar

--- src/app/shared/components/main-layout/main-layout.component.ts
+++ src/app/shared/components/main-layout/main-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/admin/services/auth.service';
 import { SharedService } from '../../services/shared.service';
 
@@ -9,14 +9,24 @@ import { SharedService } from '../../services/shared.service';
 })
 export class MainLayoutComponent implements OnInit {
 
+  // Sticky Navbar
+  isSticky: boolean = false;
+  scrollThreshold: number = 100;
+
   showInput = false
   navSearchInput = ''
+
   constructor(
     public authService: AuthService,
     private sharedService:  SharedService
   ){}
   ngOnInit(): void { 
   }
+  
+  // Sticky Navbar
+  @HostListener('window:scroll', [])
+  onScroll() {this.isSticky = window.pageYOffset > this.scrollThreshold;}
+  
   // Logout
   logout(){
     this.authService.logout()
